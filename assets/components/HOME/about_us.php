630c75b43f9d45ae0ec9384024077f6e0084c760
Fara7i <boy.doss2002@gmail.com>
2025-04-30 03:25:12 +0100

    Added second image beside the main image in about section

--- assets/components/HOME/about_us.php
+++ assets/components/HOME/about_us.php
@@ -1,35 +1,18 @@
 <div class="about-us new-section">
     <div class="about-us-content limited">
-        
+
         <div class="left-side">
             <div class="main-box">
                 <img src="<?php echo $GLOBAL_VARIABLES['hostName'] ?>/assets/imgs/backgrounds/background-1.jpg">
             </div>
 
-            <div class="box-2">
-                <div class="group">
 
-                    <div class="icon">
-                        <i class="ri-chat-1-line"></i>
-                    </div>
-                    <div class="icon">
-                        <i class="ri-chat-1-line"></i>
-                    </div>
-                </div>
-
-                <div class="icon">
-                    <i class="ri-chat-1-line"></i>
-                </div>
-                
+            <div class="main-box">
+                <img src="<?php echo $GLOBAL_VARIABLES['hostName'] ?>/assets/imgs/backgrounds/background-2.jpg">
             </div>
         </div>
 
-        <div class="right-side">
-            <?php
-            echo title($TRANSALTION_TEXTS['home_page__about_us_section__title'], $TRANSALTION_TEXTS['home_page__about_us_section__text']) 
-            ?>
-        </div>
+
 
     </div>
 </div>
-
