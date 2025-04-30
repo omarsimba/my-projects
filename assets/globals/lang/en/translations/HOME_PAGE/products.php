43ff8576508c81827284c2b08e253f645f905a32
Fara7i <boy.doss2002@gmail.com>
2025-04-30 03:37:53 +0100

    Updated products description section

--- assets/globals/lang/en/translations/HOME_PAGE/products.php
+++ assets/globals/lang/en/translations/HOME_PAGE/products.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 
 
-$home_page__products_section__title = 'Our products';
-$home_page__products_section__text = '';
+$home_page__products_section__title = 'Choose the Plan That Fits You Best';
+$home_page__products_section__text = 'Whether you\'re a casual viewer or a full-on binge-watcher — we\'ve got a plan for you. Pick the one that suits your needs and start streaming instantly.';
 
 
 $home_page__products_section__words = [
@@ -11,6 +11,3 @@ $home_page__products_section__words = [
 	'get_this_plan' => 'Get this plan',
 
 ];
-
-
-
