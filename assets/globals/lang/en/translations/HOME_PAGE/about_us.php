5fe0946b0f52c9b20a3438e2c6b7a85ac19acb35
Fara7i <boy.doss2002@gmail.com>
2025-04-30 03:01:15 +0100

    Changed about us section description text

--- assets/globals/lang/en/translations/HOME_PAGE/about_us.php
+++ assets/globals/lang/en/translations/HOME_PAGE/about_us.php
@@ -3,7 +3,7 @@
 
 
 $home_page__about_us_section__title = 'This is about us section';
-$home_page__about_us_section__text = 'text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze v text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze v text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze text text texts text sejzek zekj zer zel zel lzed ldb lzekdb ldfze v ';
+$home_page__about_us_section__text = '> At Xtreamify, we bring the world of entertainment straight to your screen.> Whether you\'re looking for the latest blockbuster movies, trending TV shows, or premium IPTV channels from around the globe — we\'ve got you covered. > Our platform offers a seamless, high-quality streaming experience tailored to your preferences. > With reliable customer support and fast access to content, Xtreamify is your go-to source for all things streaming.';
 
 
 $home_page__about_us_section__box = [
