13b6de6a388c698d472663acafeb1cd3f7f82d9a
Fara7i <boy.doss2002@gmail.com>
2025-04-30 03:03:36 +0100

    Removed small description box in about us

--- assets/components/HOME/about_us.php
+++ assets/components/HOME/about_us.php
@@ -6,16 +6,6 @@
                 <img src="<?php echo $GLOBAL_VARIABLES['hostName'] ?>/assets/imgs/backgrounds/background-1.jpg">
             </div>
 
-            <div class="box-1">
-                <div class="left-side">
-                    <i class="ri-chat-1-line"></i>
-                </div>
-                <div class="right-side">
-                    <p class="title"><?php echo $TRANSALTION_TEXTS['home_page__about_us_section__box']['title'] ?></p>
-                    <p class="text"><?php echo $TRANSALTION_TEXTS['home_page__about_us_section__box']['text'] ?></p>
-                </div>
-            </div>
-
             <div class="box-2">
                 <div class="group">
 
